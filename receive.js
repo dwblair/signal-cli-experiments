0a3002b126e12995c37360c1e08c6b84a147dfc2
Don Blair <donblair@gmail.com>
2021-02-18 19:23:47 -0500

    update

---
+++ receive.js
@@ -0,0 +1,15 @@
+const { exec } = require("child_process");
+
+var command = "signal-cli -u +18572850070 receive";
+
+exec(command, (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
